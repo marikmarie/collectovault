5ef3515f48a3ec4969462640858c1df4f662e860
marikmarie <mariamtukasingura8@gmail.com>
2025-12-13 13:29:39 +0300

    admin navbar

--- collecto-vault-webapp/src/App.tsx
+++ collecto-vault-webapp/src/App.tsx
@@ -8,6 +8,11 @@ import Login from './pages/Login';
 import Services from './pages/Admin/Services';
 import Reports from './pages/Admin/Reports';
 import LoyaltySettings from './pages/Admin/LoyaltySettings';
+import DashBoard from './pages/Admin/Dashboard';
+import Tiers from './pages/Admin/Tiers';
+import Users from './pages/Admin/UsersPage';
+import PointRules from './pages/Admin/PointRules';
+import Packages from './pages/Admin/packages';
 
 import { Routes, Route, Navigate } from "react-router-dom";
 
@@ -21,7 +26,12 @@ export default function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/services" element={<Services />} />
       <Route path="/reports" element={<Reports />} />
-      <Route path="/loyalty-settings" element={<LoyaltySettings />} />
+      <Route path="/settings" element={<LoyaltySettings />} />
+      <Route path="/admin/dashboard" element={<DashBoard />} />
+      <Route path="/tiers" element={<Tiers />} />
+      <Route path="/users" element={<Users />} />
+      <Route path="/point-rules" element={<PointRules />} />
+      <Route path="/packages" element={<Packages />} />
 
     </Routes>
   );
