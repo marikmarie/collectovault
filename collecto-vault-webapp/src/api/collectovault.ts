1a5f64c63e74b3a91bf56b1c561876ca65cfcd44
marikmarie <mariamtukasingura8@gmail.com>
2026-01-27 14:31:47 +0300

    point rules

--- collecto-vault-webapp/src/api/collectovault.ts
+++ collecto-vault-webapp/src/api/collectovault.ts
@@ -2,36 +2,27 @@
 import api from "./index";
 
 export const collectovault = {
-  getMyServices: () => api.get(`/services`),
-
-
   //point rules
   getPointRules: (vendorId: string) =>
     api.get(`/pointRules/collecto/${vendorId}/`),
-
-  getPointRulesById: (id: string, ) =>
-    api.get(`/pointRules/${id}/`),
+  getPointRulesById: (id: string) => api.get(`/pointRules/${id}/`),
   savePointRule: (vendorId: string, data: any) =>
     api.post(`/pointRules/${vendorId}`, data),
   deletePointRule: (vendorId: string, ruleId: number) =>
     api.delete(`/pointRules/${vendorId}/pointRules/${ruleId}`),
+  editletePointRule: (vendorId: string, ruleId: number) =>
+    api.delete(`/pointRules/${vendorId}/pointRules/${ruleId}`),
 
   //tier rules
-  getTierRules: (vendorId: string) =>
-    api.get(`/tier/collecto/${vendorId}`),
-  getTierRuleById: (id: string) =>
-    api.get(`/tier/${id}/`),
+  getTierRules: (vendorId: string) => api.get(`/tier/collecto/${vendorId}`),
+  getTierRuleById: (id: string) => api.get(`/tier/${id}/`),
   saveTierRule: (vendorId: string, data: any) =>
     api.post(`/tier/${vendorId}`, data),
-
 
   //point packages
   getPackages: (vendorId: number) =>
     api.get(`/vaultPackages/collecto/${vendorId}`),
-  getPackageById: (id: string) =>
-    api.get(`/vaultPackages/${id}/`),
+  getPackageById: (id: string) => api.get(`/vaultPackages/${id}/`),
   savePackages: (vendorId: string, data: any) =>
     api.post(`/vaultPackages/${vendorId}/tier-rules`, data),
-
-
 };
