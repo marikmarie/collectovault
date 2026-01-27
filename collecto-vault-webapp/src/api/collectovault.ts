2552c224baeb532d64c11da8ca2fc411bfb006ce
marikmarie <mariamtukasingura8@gmail.com>
2026-01-27 18:18:15 +0300

    new changes

--- collecto-vault-webapp/src/api/collectovault.ts
+++ collecto-vault-webapp/src/api/collectovault.ts
@@ -3,9 +3,9 @@ import api from "./index";
 
 export const collectovault = {
   //point rules
-  getPointRules: (vendorId: string) =>
-    api.get(`/pointRules/collecto/${vendorId}/`),
+  getPointRules: (vendorId: string) => api.get(`/pointRules/${vendorId}/`),
   getPointRulesById: (id: string) => api.get(`/pointRules/${id}/`),
+  
   savePointRule: (vendorId: string, data: any) =>
     api.post(`/pointRules/${vendorId}`, data),
   deletePointRule: (vendorId: string, ruleId: number) =>
@@ -14,22 +14,21 @@ export const collectovault = {
     api.delete(`/pointRules/${vendorId}/pointRules/${ruleId}`),
 
   //tier rules
-  getTierRules: (vendorId: string) => api.get(`/tier/collecto/${vendorId}`),
+  getTierRules: (vendorId: string) => api.get(`/tier/${vendorId}`),
   getTierRuleById: (id: string) => api.get(`/tier/${id}/`),
   saveTierRule: (vendorId: string, data: any) =>
     api.post(`/tier/${vendorId}`, data),
-    deleteTierRules: (vendorId: string, ruleId: number) =>
+  deleteTierRules: (vendorId: string, ruleId: number) =>
     api.delete(`/tier/${vendorId}/tier/${ruleId}`),
   editleteTierRules: (vendorId: string, ruleId: number) =>
     api.delete(`/tier/${vendorId}/tier/${ruleId}`),
 
   //point packages
-  getPackages: (vendorId: number) =>
-    api.get(`/vaultPackages/collecto/${vendorId}`),
+  getPackages: (vendorId: number) => api.get(`/vaultPackages/${vendorId}`),
   getPackageById: (id: string) => api.get(`/vaultPackages/${id}/`),
   savePackages: (vendorId: string, data: any) =>
-    api.post(`/vaultPackages/${vendorId}/packages`, data),
-    deletePackages: (vendorId: string, ruleId: number) =>
+    api.post(`/vaultPackages/${vendorId}`, data),
+  deletePackages: (vendorId: string, ruleId: number) =>
     api.delete(`/vaultPackages/${vendorId}/packages/${ruleId}`),
   editletePackages: (vendorId: string, ruleId: number) =>
     api.delete(`/vaultPackages/${vendorId}/packages/${ruleId}`),
