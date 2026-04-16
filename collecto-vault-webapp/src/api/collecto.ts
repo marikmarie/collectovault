7d8c115228a6226c5b9e5e4918ffed25bf0c266f
marikmarie <mariamtukasingura8@gmail.com>
2026-04-16 16:37:04 +0300

    change 6

--- collecto-vault-webapp/src/api/collecto.ts
+++ collecto-vault-webapp/src/api/collecto.ts
@@ -2,16 +2,6 @@ import api from "./index";
 
 
 
-export const transactionService = {
-  buyPoints: (customerId: string, data: any) =>
-    api.post(`/transactions/${customerId}/buy-points`, data),
-  redeemPoints: (customerId: string, data: any) =>
-    api.post(`/transactions/${customerId}/redeem`, data),
-  
-  getTransactions: (customerId: string, limit: number = 50, offset: number = 0) =>
-    api.post(`/transactions`, { customerId, limit, offset }),
-};
-
 export const invoiceService = {
   createInvoice: (payload: any) => api.post("/invoice", payload),
 
